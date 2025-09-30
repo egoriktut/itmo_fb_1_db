e6f26b3be296706f1c3573e93fec0e0f424e073e
Egor Cherevko <e.cherevko@tm-industries.ru>
2025-09-30 12:44:44 +0300

    fix

--- SQL/vrum-vrum/init-sqript.sql
+++ SQL/vrum-vrum/init-sqript.sql
@@ -14,7 +14,9 @@ CREATE table raw_data.sales (
     brand_origin VARCHAR(50)
 );
 
-
+-- Код наполнения  сырой таблицы стоило оставить, чтобы можно было весь скрип целиком выполнить.
+--     ОТВЕТ:
+-- Заполнял через импорт в ide, вроде так можно было :)
 -- -- -- -- --
 
 
@@ -42,6 +44,16 @@ CREATE table car_shop.colors (
 --  (А то если начать придумывать и масштабировать будет слишком много....)
 --  countries и colors в другую схему,
 --  по типу extra_information
+-- Почему в другую схему?
+-- Можешь поподробнее расписать. Не совсем понял, что ты имеешь ввиду.
+--     ОТВЕТ:
+-- Я считаю, что схемы должны логический делить таблицы на подгруппы, и так как схема car_shop является схемой автосалона,
+-- то к ней не имеют отношения цвета и старны, как в примере.
+-- У меня была похожая тема на КР в вузе, а именно детейлинг-центр для авто. И я сделал вот такое разбиение.
+-- https://github.com/egoriktut/detail_center_db_kursach_8/blob/main/init-scripts/02-create-schemas.sql
+-- Правда в проде отдельно я такого не встречал, обычно все пишут в одной схеме
+-- Интересно узнать, что ты думаешь на этот счет, излишки это или нет.
+
 
 CREATE table car_shop.customers (
     id SERIAL PRIMARY KEY,
@@ -63,29 +75,28 @@ CREATE table car_shop.brand_models (
     id SERIAL PRIMARY KEY,
     brand_id INTEGER REFERENCES car_shop.brands (id) on delete cascade,
     -- Самое длинное название модели авто Lamborghini Murciélago LP 670-4 SuperVeloce
-    name varchar(43) CHECK ( name ~ '^[A-Za-z0-9\s\-\.]+$' ) not null
+    name varchar(43) CHECK ( name ~ '^[A-Za-z0-9\s\-\.]+$' ) not null,
+    -- Когда говорят про расход, не называют больше одной цифры после ','.
+    -- И не бывает расхода у НОРМАЛЬНЫХ машин, больше 100
+    gasoline_consumption NUMERIC(3, 1) CHECK (gasoline_consumption BETWEEN 0 and 99)
 );
 
 create table car_shop.cars (
     id SERIAL PRIMARY KEY,
-    -- Адекватная цена, до 7 знаков, два знака после запятой, так во всех валютах. Цена не нулевая
-    price NUMERIC(9, 2) CHECK (price > 0),
     car_name_id INTEGER REFERENCES car_shop.brand_models (id) on delete cascade,
-    color_id INTEGER REFERENCES car_shop.colors (id) on delete set null,
-    -- Когда говорят про расход, не называют больше одной цифры после ','.
-    -- И не бывает расхода у НОРМАЛЬНЫХ машин, больше 100
-    gasoline_consumption NUMERIC(3, 1) CHECK (gasoline_consumption BETWEEN 0 and 99),
-    date_manufacture DATE not null
+    color_id INTEGER REFERENCES car_shop.colors (id) on delete set null
 );
 
 CREATE table car_shop.sales (
     id SERIAL PRIMARY KEY,
     car_id INTEGER REFERENCES car_shop.cars (id) on delete cascade,
     customer_id INTEGER REFERENCES car_shop.customers (id) on delete cascade,
+    -- Адекватная цена, до 7 знаков, два знака после запятой, так во всех валютах. Цена не нулевая
+    price NUMERIC(9, 2) CHECK (price > 0),
     -- Скида 0-100 %
     discount SMALLINT CHECK ( discount BETWEEN 0 and 100),
     -- По умолчанию проставляем дату продажи сейчас
-    date DATE default CURRENT_TIMESTAMP
+    date DATE default CURRENT_TIMESTAMP not null
 );
 
 -- Вставка данных из raw_data
@@ -124,129 +135,74 @@ INSERT INTO
     car_shop.brands (name, origin_id)
 SELECT DISTINCT
     SUBSTRING(auto FROM 0 FOR POSITION(' ' IN auto)) as brand,
-    (
-        SELECT id FROM car_shop.countries WHERE name = c.brand_origin
-    )
+    country.id
 FROM
     raw_data.sales c
+JOIN
+    car_shop.countries country ON name =c.brand_origin
 WHERE c.brand_origin IS NOT NULL AND c.auto IS NOT NULL;
 
 
 INSERT INTO
-    car_shop.brand_models (name, brand_id)
+    car_shop.brand_models (name, brand_id, gasoline_consumption)
 SELECT DISTINCT
     SUBSTRING(
             auto FROM POSITION(' ' IN auto) + 1 FOR (
         POSITION(',' IN auto) - POSITION(' ' IN auto) - 1
         )
     ) as model,
-    (
-        SELECT
-            id
-        FROM
-            car_shop.brands
-        WHERE name = SUBSTRING(auto FROM 0 FOR POSITION(' ' IN auto))
-    ) as br_id
+    brand.id,
+    gasoline_consumption
 FROM
     raw_data.sales c
+JOIN
+    car_shop.brands brand ON brand.name = SUBSTRING(auto FROM 0 FOR POSITION(' ' IN auto))
 WHERE c.brand_origin IS NOT NULL AND c.auto IS NOT NULL;
 
 
 INSERT INTO
-    car_shop.cars (price, car_name_id, color_id, gasoline_consumption, date_manufacture)
+    car_shop.cars (car_name_id, color_id)
 SELECT DISTINCT
-    price,
-    (
-        SELECT
-            id
-        FROM
-            car_shop.brand_models b_m
-        WHERE
-            brand_id =
-                (
-                    SELECT
-                        id
-                    FROM
-                        car_shop.brands br
-                    WHERE br.name = SUBSTRING(c.auto FROM 0 FOR POSITION(' ' IN c.auto))
-                )
-            AND
-            b_m.name =
-                (
-                    SUBSTRING(
-                            c.auto FROM POSITION(' ' IN c.auto) + 1 FOR (
-                            POSITION(',' IN c.auto) - POSITION(' ' IN c.auto) - 1
-                        )
-                    )
-                )
-    ),
-    (
-        SELECT
-            id
-        FROM
-            car_shop.colors color
-        WHERE
-            color.name = SUBSTRING(c.auto FROM POSITION(',' IN c.auto) + 2)
-    ),
-    COALESCE(gasoline_consumption::numeric, 0.0),
-    date
+    b_m.id,
+    color.id
 FROM
-    raw_data.sales c;
+    raw_data.sales c
+JOIN
+    car_shop.brand_models b_m ON b_m.name =
+        SUBSTRING(
+            c.auto FROM POSITION(' ' IN c.auto) + 1 FOR (
+                POSITION(',' IN c.auto) - POSITION(' ' IN c.auto) - 1
+            )
+        )
+JOIN
+    car_shop.colors color ON color.name = SUBSTRING(c.auto FROM POSITION(',' IN c.auto) + 2) ;
 
 
 INSERT INTO
-    car_shop.sales (car_id, customer_id, discount)
+    car_shop.sales (car_id, customer_id, price, discount, date)
 SELECT DISTINCT
-    (
-        SELECT
-            id
-        FROM
-            car_shop.cars car
-        WHERE
-            car.car_name_id =
-                (
-                    SELECT
-                        id
-                    FROM
-                        car_shop.brand_models b_m
-                    WHERE
-                        brand_id =
-                            (
-                                SELECT
-                                    id
-                                FROM
-                                    car_shop.brands br
-                                WHERE br.name = SUBSTRING(c.auto FROM 0 FOR POSITION(' ' IN c.auto))
-                            )
-                      AND
-                        b_m.name =
-                            (
-                                SUBSTRING(
-                                        c.auto FROM POSITION(' ' IN c.auto) + 1 FOR (
-                                    POSITION(',' IN c.auto) - POSITION(' ' IN c.auto) - 1
-                                    )
-                                )
-                            )
-                )
-            AND
-            car.price = c.price
-            AND
-            car.date_manufacture = c.date
-    ) AS car_id,
-    (
-        SELECT DISTINCT
-            id
-        FROM
-            car_shop.customers cust
-        WHERE
-            cust.full_name = person_name
-            AND
-            cust.phone = c.phone
-    ) as customer_id,
-    discount
+    car.id,
+    customer.id,
+    c.price,
+    discount,
+    date
 FROM
-    raw_data.sales c;
-
+    raw_data.sales c
+JOIN
+    car_shop.customers customer ON
+        customer.full_name = person_name AND customer.phone = c.phone
+JOIN car_shop.brand_models model ON model.name =
+    SUBSTRING(
+        auto FROM POSITION(' ' IN auto) + 1 FOR (
+            POSITION(',' IN auto) - POSITION(' ' IN auto) - 1
+        )
+    )
+JOIN
+    car_shop.colors color ON color.name = SUBSTRING(c.auto FROM POSITION(',' IN c.auto) + 2)
+JOIN
+    car_shop.cars car ON car.car_name_id = model.id
+WHERE
+    car.color_id = color.id;
 
 
 -- ЭТАП 2
@@ -254,9 +210,9 @@ FROM
 -- Запрос, который выведет процент моделей машин, у которых нет параметра gasoline_consumption
 SELECT
     (
-        SELECT COUNT(*) FROM car_shop.cars WHERE gasoline_consumption = 0.0
+        SELECT COUNT(*) FROM car_shop.brand_models WHERE COALESCE(gasoline_consumption, 0.0) = 0.0
     )::numeric / (
-        SELECT COUNT(*) FROM car_shop.cars
+        SELECT COUNT(*) FROM car_shop.brand_models
     ) * 100
     AS nulls_percentage_gasoline_consumption;
 
@@ -270,7 +226,6 @@ AS
         concat(brand.name, ' ', bm.name) AS full_name_model,
         brand.name AS brand_name,
         bm.name AS model,
-        date_manufacture,
         date,
         color_id,
         price,
@@ -291,7 +246,7 @@ AS
 --  Среднюю цену округлите до второго знака после запятой.
 SELECT
     brand_name,
-    EXTRACT(YEAR FROM date_manufacture) as year,
+    EXTRACT(YEAR FROM date) as year,
     ROUND(AVG(price), 2)
 FROM
     car_shop.cars_with_info
@@ -304,13 +259,13 @@ ORDER BY
 -- Результат отсортируйте по месяцам в восходящем порядке.
 -- Среднюю цену округлите до второго знака после запятой.
 SELECT
-    EXTRACT(MONTH FROM date_manufacture) as month,
+    EXTRACT(MONTH FROM date) as month,
     '2022' AS year,
     ROUND(AVG(price), 2)
 FROM
     car_shop.cars_with_info
 WHERE
-    EXTRACT(YEAR FROM date_manufacture) = '2022'
+    EXTRACT(YEAR FROM date) = '2022'
 GROUP BY
     month;
 
@@ -332,8 +287,8 @@ GROUP BY cust.full_name;
 -- без учёта скидки. Цена в колонке price дана с учётом скидки.
 SELECT
     countries.name AS brand_origin,
-    MAX(price + price * 0.01 * discount) as price_max,
-    MIN(price + price * 0.01 * discount) as price_min
+    MAX(price / (1 - 0.01 * discount)) as price_max,
+    MIN(price / (1 - 0.01 * discount)) as price_min
 FROM
     car_shop.cars_with_info
 JOIN
